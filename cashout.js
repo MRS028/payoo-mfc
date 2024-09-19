fe2aeca8a6fdfc0b13ff3f2291e5112a22d5ff19
SheikhRifat28 <rafayet028@gmail.com>
2024-09-19 17:35:12 +0600

    Cash out limitations added

--- cashout.js
+++ cashout.js
@@ -1,18 +1,25 @@
-document.getElementById('btnCashout').addEventListener('click',function(event){
+document
+  .getElementById("btnCashout")
+  .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const cashOutInput = document.getElementById('cashout-input').value;
-    
-    const cashOutpin = document.getElementById('cashOut-pin').value;
+    const cashOutInput = document.getElementById("cashout-input").value;
+
+    const cashOutpin = document.getElementById("cashOut-pin").value;
     console.log(cashOutInput, cashOutpin);
-    if(cashOutpin === '1234'){
+    const mainBlance = document.getElementById("account-balance").innerText;
+
+    if (cashOutpin === "1234") {
+      if (parseFloat(cashOutInput) > parseFloat(mainBlance)) {
+        alert("You have not sufficient balance for cash out.");
+      } else if (parseFloat(mainBlance) > parseFloat(cashOutInput)) {
         console.log("cash out is continuing from your account");
-        const balance = document.getElementById('account-balance').innerText;
+        const balance = document.getElementById("account-balance").innerText;
         const newBalance = parseFloat(balance) - parseFloat(cashOutInput);
         console.log(newBalance);
-        document.getElementById('account-balance').innerText = newBalance;
+        document.getElementById("account-balance").innerText = newBalance;
+      }
+    } else {
+      alert("Invalid Pin");
     }
-    else{
-        alert('Invalid Pin');
-    }
-})
+  });
